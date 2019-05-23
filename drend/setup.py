915994b76d95d37c6825776cda839478d0f842d0
Meysam Azad <MeysamAzad81@yahoo.com>
2019-05-24 00:06:14 +0430

    initial setup.py for the project

---
+++ drend/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='Drend',
+    version='1.0.0',
+    description='Gathering my knowledge into practical tangible product',
+    author='Meysam Azad',
+    author_email='MeysamAzad81@yahoo.com',
+    install_requires=[
+        "Django==2.2.1",
+        "gunicorn==19.9.0",
+    ],
+    license='MIT',
+    keywords=[
+        'python',
+        'django',
+    ],
+)
