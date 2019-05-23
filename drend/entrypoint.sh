f6f0d448657cb716e3ea33ea01085394d44250d7
Meysam Azad <MeysamAzad81@yahoo.com>
2019-05-24 00:06:57 +0430

    initial Dockerfile & entrypoint

---
+++ drend/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+gunicorn -b ${ADDRESS} \
+         --backlog ${BACKLOG} \
+         --threads ${THREADS} \
+         --max-requests ${MAX_REQUESTS} \
+         -t ${TIMEOUT} \
+         --keep-alive ${KEEP_ALIVE} \
+         --limit-request-line ${LIMIT_REQUEST_LINE} \
+         ${RELOAD} \
+         ${NO_SENDFILE} \
+         ${DAEMON} \
+         --log-level ${LOG_LEVEL} \
+         -n ${NAME} \
+         drend.wsgi
