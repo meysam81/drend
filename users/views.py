2ee20f71ee28867f037d88df2261186dce6850bc
Meysam Azad <MeysamAzad81@yahoo.com>
2019-05-26 21:44:17 +0430

    a demonstration of DRF; to be completed later

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.viewsets import ModelViewSet
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewset(ModelViewSet):
+    queryset = User.objects.order_by('-date_joined')
+    serializer_class = UserSerializer
