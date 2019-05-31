f0a43d95594bc9e9f55ff6988c6f6a45331b5224
Meysam Azad <MeysamAzad81@yahoo.com>
2019-05-31 21:25:08 +0430

    add mongo as database

--- drend/setup.py
+++ drend/setup.py
@@ -12,6 +12,8 @@ setup(
         'djangorestframework==3.9.4',
         'Markdown==3.1.1',
         'django-filter==2.1.0',
+        'djongo==1.2.32',
+        'sqlparse==0.2.4',
     ],
     license='MIT',
     keywords=[
