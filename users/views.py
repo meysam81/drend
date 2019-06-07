2a8fef6749e0d06765617968c7d54c6bdac32cfa
Meysam Azad <MeysamAzad81@yahoo.com>
2019-06-07 12:24:25 +0430

    restrict authorization to the token itself

--- users/views.py
+++ users/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import BasePermission
 from .serializers import UserSerializer
 
 
 class UserViewset(ModelViewSet):
+
+    class IsOwnerOrReadOnly(BasePermission):
+        def has_object_permission(self, request, view, obj):
+            return bool(obj == request.user)
+
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = User.objects.order_by('-date_joined')
     serializer_class = UserSerializer
     lookup_field = 'username'
