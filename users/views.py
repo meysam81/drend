dd9ca2c5e2eaf0e2051a58c577807a0f1bfcd392
Meysam Azad <MeysamAzad81@yahoo.com>
2019-06-07 18:06:55 +0430

    change permission class name to `IsOwner` and move it to permissions.py

--- users/views.py
+++ users/views.py
@@ -1,17 +1,14 @@
 from django.contrib.auth.models import User
+
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import BasePermission
+
+
+from .permissions import IsOwner
 from .serializers import UserSerializer
 
 
 class UserViewset(ModelViewSet):
-
-    class IsOwnerOrReadOnly(BasePermission):
-        def has_object_permission(self, request, view, obj):
-            return bool(obj == request.user)
-
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsOwner,)
     queryset = User.objects.order_by('-date_joined')
     serializer_class = UserSerializer
     lookup_field = 'username'
-
