ae607324d6304c9f43fc1fe1258f90936ad2635d
Meysam Azad <MeysamAzad81@yahoo.com>
2019-06-09 23:21:36 +0430

    users serializers: email is required; also hash input password
    why didn't it hash automatically surprised me though

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    @staticmethod
+    def validate_password(password: str) -> str:
+        return make_password(password)
+
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {
+            'email': {'required': True},
+        }
